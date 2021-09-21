a68b638eaa2a8730d81c3ec59a1a8c892d6efb46
willian-silva7 <willian.1994@hotmail.com>
2021-09-21 23:12:37 +0100

    added new containers

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-alert('hello world')
+const nav = document.querySelector("#header nav")
+const toggle = document.querySelectorAll("nav .toggle")
+
+for (const element of toggle){
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+  link.addEventListener('click', function() {
+    nav.classList.remove('show')
+  })
+}
+
+// Alterar header da página quando houver scroll
+
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if(window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
